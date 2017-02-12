01e75f5d12d736e91566dfc7641e23082e5fcb1f
daemon369 <daemon336699@gmail.com>
2017-02-12 17:34:41 +0800

    实现倒计时及点击退出功能

--- app/src/main/java/me/daemon/splashview/MainActivity.java
+++ app/src/main/java/me/daemon/splashview/MainActivity.java
@@ -2,10 +2,13 @@ package me.daemon.splashview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import me.daemon.splashview.demo.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         final Splash<String> splash = new Splash.Builder<String>(this)
                 .imageUrl("http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg")
                 .tag("http://www.baidu.com")
+                .duration(5)
+                .callback(new Splash.Callback<String>() {
+                    @Override
+                    public void onSplashImageClicked(Splash<String> splash, String tag) {
+                        Log.e(TAG, "onSplashImageClicked");
+                    }
+
+                    @Override
+                    public void onSplashSkipClicked(Splash<String> splash, String tag) {
+                        Log.e(TAG, "onSplashSkipClicked");
+                    }
+
+                    @Override
+                    public void onSplashFinish(Splash<String> splash, String tag) {
+                        Log.e(TAG, "onSplashFinish");
+                    }
+                })
                 .build();
         splash.show(this);
     }
--- splashview/src/main/java/me/daemon/splashview/Splash.java
+++ splashview/src/main/java/me/daemon/splashview/Splash.java
@@ -10,11 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author daemon369
@@ -24,21 +24,21 @@ public class Splash<T> {
 
     private final static String TAG = Splash.class.getSimpleName();
 
-    private final Integer layoutId;
-    private final View view;
     private final Integer duration;
     private final String imageUrl;
     private final T tag;
-    private View splashView;
-    private ImageView imageView;
-    private Button skip;
+    private final View splashView;
+    private final ImageView imageView;
+    private final TextView timeView;
+    private final View skipView;
+    private final Callback<T> callback;
+    private WeakReference<Activity> activityRef;
 
-    private Splash(final Context context, final Integer layoutId, final View view, final Integer duration, final String imageUrl, final T tag) {
-        this.layoutId = layoutId;
-        this.view = view;
+    private Splash(final Context context, final Integer layoutId, final View view, final Integer duration, final String imageUrl, final T tag, Callback<T> callback) {
         this.duration = duration;
         this.imageUrl = imageUrl;
         this.tag = tag;
+        this.callback = callback;
 
         if (null != layoutId) {
             splashView = LayoutInflater.from(context).inflate(layoutId, null);
@@ -53,15 +53,83 @@ public class Splash<T> {
             throw new IllegalStateException("can't find an image view with id R.id.splash_view_image");
         }
 
-        skip = (Button) splashView.findViewById(R.id.splash_view_skip);
-        if (null == skip) {
-            throw new IllegalStateException("can't find a button with id R.id.splash_view_skip");
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hide();
+                if (null != Splash.this.callback) {
+                    Splash.this.callback.onSplashImageClicked(Splash.this, tag);
+                }
+            }
+        });
+
+        timeView = (TextView) splashView.findViewById(R.id.splash_view_time);
+        if (null == timeView) {
+            throw new IllegalStateException("can't find a text view with id R.id.splash_view_time");
         }
 
+        skipView = splashView.findViewById(R.id.splash_view_skip);
+        if (null == skipView) {
+            throw new IllegalStateException("can't find a view with id R.id.splash_view_skip");
+        }
+
+        skipView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hide();
+                if (null != Splash.this.callback) {
+                    Splash.this.callback.onSplashSkipClicked(Splash.this, tag);
+                }
+            }
+        });
     }
 
-    public boolean isActivityAlive(final Activity activity) {
+    public View getSplashView() {
+        return splashView;
+    }
+
+    public T getTag() {
+        return tag;
+    }
+
+    private boolean isActivityAlive(final Activity activity) {
         return null != activity && !activity.isFinishing();
+    }
+
+    private void showInternal(final Activity activity) {
+        if (isActivityAlive(activity)) {
+            final ViewGroup content = (ViewGroup) activity.getWindow().getDecorView().findViewById(android.R.id.content);
+            final LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+            content.addView(splashView, lp);
+
+            if (null != duration && duration > 0) {
+                timeView.setVisibility(View.VISIBLE);
+                timeView.setText(String.valueOf(duration));
+                final Runnable countDown = new Runnable() {
+
+                    int leftOver = duration;
+
+                    @Override
+                    public void run() {
+                        leftOver--;
+                        if (leftOver > 0) {
+                            timeView.setText(String.valueOf(leftOver));
+                            splashView.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+                        } else {
+                            hide();
+                            if (null != callback) {
+                                callback.onSplashFinish(Splash.this, tag);
+                            }
+                        }
+                    }
+                };
+
+                splashView.postDelayed(countDown, TimeUnit.SECONDS.toMillis(1));
+            } else {
+                timeView.setVisibility(View.GONE);
+            }
+        }
     }
 
     public void show(final Activity activity) {
@@ -70,46 +138,31 @@ public class Splash<T> {
             return;
         }
 
-        final Runnable showTask = new Runnable() {
-            @UiThread
-            @Override
-            public void run() {
-                if (isActivityAlive(activity)) {
-                    final ViewGroup content = (ViewGroup) activity.getWindow().getDecorView().findViewById(android.R.id.content);
-                    LayoutParams lp = null;
-                    if (content instanceof FrameLayout) {
-                        lp = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                    } else if (content instanceof RelativeLayout) {
-                        lp = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                    } else if (content instanceof LinearLayout) {
-                        lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                    }
-
-                    if (null == lp) {
-                        throw new IllegalStateException("unrecognized ViewGroup: " + content.getClass().getCanonicalName());
-                    }
-
-                    content.addView(splashView, lp);
-                }
-            }
-        };
+        activityRef = new WeakReference<>(activity);
 
         if (isActivityAlive(activity)) {
             if (Looper.getMainLooper() == Looper.myLooper()) {
-                showTask.run();
+                showInternal(activity);
             } else {
-                activity.runOnUiThread(showTask);
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showInternal(activity);
+                    }
+                });
             }
         } else {
             Log.i(TAG, "activity is not alive: " + activity);
         }
     }
 
-    public void hide(final Activity activity) {
+    public void hide() {
         if (null == splashView.getParent()) {
             Log.i(TAG, "SplashView is not showing yet");
             return;
         }
+
+        final Activity activity = null != activityRef ? activityRef.get() : null;
 
         final Runnable showTask = new Runnable() {
             @UiThread
@@ -133,6 +186,15 @@ public class Splash<T> {
         }
     }
 
+    public interface Callback<T> {
+
+        void onSplashImageClicked(final Splash<T> splash, final T tag);
+
+        void onSplashSkipClicked(final Splash<T> splash, final T tag);
+
+        void onSplashFinish(final Splash<T> splash, final T tag);
+    }
+
     public static class Builder<T> {
         private final Context context;
         private Integer layoutId;
@@ -140,6 +202,7 @@ public class Splash<T> {
         private Integer duration;
         private String imageUrl;
         private T tag;
+        private Callback<T> callback;
 
         public Builder(Context context) {
             if (null == context) {
@@ -186,7 +249,7 @@ public class Splash<T> {
                 throw new IllegalArgumentException("imageUrl must not be empty");
             }
             if (null != this.imageUrl) {
-                throw new IllegalArgumentException("imageUrl already set");
+                throw new IllegalStateException("imageUrl already set");
             }
             this.imageUrl = imgUrl;
             return this;
@@ -200,8 +263,16 @@ public class Splash<T> {
             return this;
         }
 
+        public Builder<T> callback(final Callback<T> callback) {
+            if (null != this.callback && null != callback) {
+                throw new IllegalStateException("callback already set");
+            }
+            this.callback = callback;
+            return this;
+        }
+
         public Splash<T> build() {
-            return new Splash<T>(context, layoutId, view, duration, imageUrl, tag);
+            return new Splash<T>(context, layoutId, view, duration, imageUrl, tag, callback);
         }
     }
 }
